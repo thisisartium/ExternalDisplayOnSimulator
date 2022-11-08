d7ffa47e9d55b60d22435b7928682057b0dfc390
Paul Zabelin <paulz@users.noreply.github.com>
2022-11-07 17:32:45 -0800

    Initial Commit

---
+++ ExternalDisplayTest/ExternalDisplayTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExternalDisplayTestApp.swift
+//  ExternalDisplayTest
+//
+//  Created by Artium on 11/7/22.
+//
+
+import SwiftUI
+
+@main
+struct ExternalDisplayTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExternalDisplayTestUITests/ExternalDisplayTestUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ExternalDisplayTestUITestsLaunchTests.swift
+//  ExternalDisplayTestUITests
+//
+//  Created by Artium on 11/7/22.
+//
+
+import XCTest
+
+final class ExternalDisplayTestUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
