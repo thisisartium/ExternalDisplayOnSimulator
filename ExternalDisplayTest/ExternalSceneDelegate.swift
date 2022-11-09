da94a7df08368e26cfb6f753dfcd7b2fe3751f07
Paul Zabelin <paulz@users.noreply.github.com>
2022-11-09 11:09:22 -0800

    use higher resolution if available

--- ExternalDisplayTest/ExternalSceneDelegate.swift
+++ ExternalDisplayTest/ExternalSceneDelegate.swift
@@ -7,13 +7,28 @@ class ExternalSceneDelegate: UIResponder, UIWindowSceneDelegate {
       return
     }
     let screen = windowScene.screen
-    print("availableModes", screen.availableModes)
+    if screen.availableModes.count > 1 {
+      let higherResolution = screen.availableModes.max {
+        $0.size > $1.size
+      }!
+      print("switching to higher resolution mode", higherResolution)
+      screen.currentMode = higherResolution
+    }
     print("currentMode", screen.currentMode!)
-    print("preferredMode", screen.preferredMode!)
+//    print("preferredMode", screen.preferredMode!)
     print("bounds", screen.bounds)
     print("nativeBounds", screen.nativeBounds)
-    print("nativeScale", screen.nativeScale)
-    print("scale", screen.scale)
-//    screen.currentMode = screen.availableModes.last!
+//    print("nativeScale", screen.nativeScale)
+//    print("scale", screen.scale)
+  }
+}
+
+extension CGSize: Comparable {
+  public static func < (lhs: CGSize, rhs: CGSize) -> Bool {
+    lhs.halfPerimeter() < rhs.halfPerimeter()
+  }
+
+  func halfPerimeter() -> Double {
+    height + width
   }
 }
